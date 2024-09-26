46755eba480f994e59671cf13ffadd102c6753c7
SnyprDragun <subho02.dc@gmail.com>
2024-09-26 21:18:39 +0530

    stay depth added

--- action_classes.py
+++ action_classes.py
@@ -464,6 +464,7 @@ class STAY(TASK):
             self.x1 = x1
             self.x2 = x2
             self.callable = 1
+            self.local_setpoint = [self.x1, self.x2]
         elif x1 is not None and x2 is None:
             self.callable = 1.5
 
@@ -471,6 +472,7 @@ class STAY(TASK):
             self.y1 = y1
             self.y2 = y2
             self.callable = 2
+            self.local_setpoint = [self.x1, self.x2, self.y1, self.y2]
         elif y1 is not None and y2 is None:
             self.callable = 2.5
 
@@ -478,6 +480,7 @@ class STAY(TASK):
             self.z1 = z1
             self.z2 = z2
             self.callable = 3
+            self.local_setpoint = [self.x1, self.x2, self.y1, self.y2, self.z1, self.z2]
         elif z1 is not None and z2 is None:
             self.callable = 3.5
 
@@ -492,53 +495,103 @@ class STAY(TASK):
 
     def checkCallableAndCallExecute(self):
         if self.callable == 1:
-            return self.execute_stay_1D()
+            if self.depth == "minimum":
+                return self.execute_stay_1D_depth_minimum()
+            elif self.depth == "partial":
+                return self.execute_stay_1D_depth_partial()
+            elif self.depth == "full":
+                return self.execute_stay_1D_depth_full()
+            else:
+                raise ValueError(f"Invalid depth value: {self.depth}. Must be one of {list(self.depths)}")
+        
         elif self.callable == 2:
-            return self.execute_stay_2D()
+            if self.depth == "minimum":
+                return self.execute_stay_2D_depth_minimum()
+            elif self.depth == "partial":
+                return self.execute_stay_2D_depth_partial()
+            elif self.depth == "full":
+                return self.execute_stay_2D_depth_full()
+            else:
+                raise ValueError(f"Invalid depth value: {self.depth}. Must be one of {list(self.depths)}")
+        
         else:
-            return self.execute_stay_3D()
+            if self.depth == "minimum":
+                return self.execute_stay_3D_depth_minimum()
+            elif self.depth == "partial":
+                return self.execute_stay_3D_depth_partial()
+            elif self.depth == "full":
+                return self.execute_stay_3D_depth_full()
+            else:
+                raise ValueError(f"Invalid depth value: {self.depth}. Must be one of {list(self.depths)}")
 
-    def execute_stay_1D(self):
+    def execute_stay_1D_depth_minimum(self):
         self.main.setpoints.append([self.x1, self.x2, self.t1, self.t2])
         all_constraints = []
         t_values = np.arange(self.t1, self.t2, self.main._step)
         lambda_ = 0.5
+    
         for t in t_values:
-        #     for lambda_1 in self.main.lambda_values:
-        #         gamma1_L = self.main.gammas(t)[0]
-        #         gamma1_U = self.main.gammas(t)[1]
-
-        #         x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
-        #         constraint = z3.And(x<self.x2, x>self.x1)
-        #         all_constraints.append(constraint)
             gamma1_L = self.main.gammas(t)[0]
             gamma1_U = self.main.gammas(t)[1]
 
-            x = (lambda_ * gamma1_L + (1 - lambda_) * gamma1_U)
-            constraint = z3.And(x<self.x2, x>self.x1)
+            x_mid = (lambda_ * gamma1_L + (1 - lambda_) * gamma1_U)
+            constraint = z3.And(x_mid<self.x2, x_mid>self.x1)
             all_constraints.append(constraint)
+
         print("Added Stay Constraints: ", self.main.setpoints)
         end = time.time()
         self.main.displayTime(self.start, end)
         return all_constraints
 
-    def execute_stay_2D(self):
+    def execute_stay_1D_depth_partial(self):
+        self.main.setpoints.append([self.x1, self.x2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        lambda_low = 0
+        lambda_high = 1
+
+        for t in t_values:
+            gamma1_L = self.main.gammas(t)[0]
+            gamma1_U = self.main.gammas(t)[1]
+
+            x_low = (lambda_low * gamma1_L + (1 - lambda_low) * gamma1_U)
+            constraint_low = z3.And(x_low<self.x2, x_low>self.x1)
+            all_constraints.append(constraint_low)
+
+            x_high = (lambda_high * gamma1_L + (1 - lambda_high) * gamma1_U)
+            constraint_high = z3.And(x_high<self.x2, x_high>self.x1)
+            all_constraints.append(constraint_high)
+
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+    
+    def execute_stay_1D_depth_full(self):
+        self.main.setpoints.append([self.x1, self.x2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        for t in t_values:
+            for lambda_1 in self.main.lambda_values:
+                gamma1_L = self.main.gammas(t)[0]
+                gamma1_U = self.main.gammas(t)[1]
+
+                x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
+                constraint = z3.And(x<self.x2, x>self.x1)
+                all_constraints.append(constraint)
+
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+
+    def execute_stay_2D_depth_minimum(self):
         self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.t1, self.t2])
         all_constraints = []
         t_values = np.arange(self.t1, self.t2, self.main._step)
         lambda_ = 0.5
-        for t in t_values:
-            # for lambda_1 in self.main.lambda_values:
-            #     for lambda_2 in self.main.lambda_values:
-            #         gamma1_L = self.main.gammas(t)[0]
-            #         gamma2_L = self.main.gammas(t)[1]
-            #         gamma1_U = self.main.gammas(t)[2]
-            #         gamma2_U = self.main.gammas(t)[3]
 
-            #         x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
-            #         y = (lambda_2 * gamma2_L + (1 - lambda_2) * gamma2_U)
-            #         constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1)
-            #         all_constraints.append(constraint)
+        for t in t_values:
             gamma1_L = self.main.gammas(t)[0]
             gamma2_L = self.main.gammas(t)[1]
             gamma1_U = self.main.gammas(t)[2]
@@ -548,32 +601,67 @@ class STAY(TASK):
             y = (lambda_ * gamma2_L + (1 - lambda_) * gamma2_U)
             constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1)
             all_constraints.append(constraint)
+
         print("Added Stay Constraints: ", self.main.setpoints)
         end = time.time()
         self.main.displayTime(self.start, end)
         return all_constraints
 
-    def execute_stay_3D(self):
+    def execute_stay_2D_depth_partial(self):
+        self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        lambda_low = 0
+        lambda_high = 1
+
+        for t in t_values:
+            gamma1_L = self.main.gammas(t)[0]
+            gamma2_L = self.main.gammas(t)[1]
+            gamma1_U = self.main.gammas(t)[2]
+            gamma2_U = self.main.gammas(t)[3]
+
+            x_low = (lambda_low * gamma1_L + (1 - lambda_low) * gamma1_U)
+            y_low = (lambda_low * gamma2_L + (1 - lambda_low) * gamma2_U)
+            constraint_low = z3.And(x_low<self.x2, x_low>self.x1, y_low<self.y2, y_low>self.y1)
+            all_constraints.append(constraint_low)
+
+            x_high = (lambda_high * gamma1_L + (1 - lambda_high) * gamma1_U)
+            y_high = (lambda_high * gamma2_L + (1 - lambda_high) * gamma2_U)
+            constraint_high = z3.And(x_high<self.x2, x_high>self.x1, y_high<self.y2, y_high>self.y1)
+            all_constraints.append(constraint_high)
+
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+
+    def execute_stay_2D_depth_full(self):
+        self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        for t in t_values:
+            for lambda_1 in self.main.lambda_values:
+                for lambda_2 in self.main.lambda_values:
+                    gamma1_L = self.main.gammas(t)[0]
+                    gamma2_L = self.main.gammas(t)[1]
+                    gamma1_U = self.main.gammas(t)[2]
+                    gamma2_U = self.main.gammas(t)[3]
+
+                    x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
+                    y = (lambda_2 * gamma2_L + (1 - lambda_2) * gamma2_U)
+                    constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1)
+                    all_constraints.append(constraint)
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+
+    def execute_stay_3D_depth_minimum(self):
         self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.z1, self.z2, self.t1, self.t2])
         all_constraints = []
         t_values = np.arange(self.t1, self.t2, self.main._step)
         lambda_ = 0.5
         for t in t_values:
-            # for lambda_1 in self.main.lambda_values:
-            #     for lambda_2 in self.main.lambda_values:
-            #         for lambda_3 in self.main.lambda_values:
-            #             gamma1_L = self.main.gammas(t)[0]
-            #             gamma2_L = self.main.gammas(t)[1]
-            #             gamma3_L = self.main.gammas(t)[2]
-            #             gamma1_U = self.main.gammas(t)[3]
-            #             gamma2_U = self.main.gammas(t)[4]
-            #             gamma3_U = self.main.gammas(t)[5]
-
-            #             x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
-            #             y = (lambda_2 * gamma2_L + (1 - lambda_2) * gamma2_U)
-            #             z = (lambda_3 * gamma3_L + (1 - lambda_3) * gamma3_U)
-            #             constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1, z<self.z2, z>self.z1)
-            #             all_constraints.append(constraint)
             gamma1_L = self.main.gammas(t)[0]
             gamma2_L = self.main.gammas(t)[1]
             gamma3_L = self.main.gammas(t)[2]
@@ -586,6 +674,64 @@ class STAY(TASK):
             z = (lambda_ * gamma3_L + (1 - lambda_) * gamma3_U)
             constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1, z<self.z2, z>self.z1)
             all_constraints.append(constraint)
+
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+    
+    def execute_stay_3D_depth_partial(self):
+        self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.z1, self.z2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        lambda_low = 0
+        lambda_high = 1
+        for t in t_values:
+            gamma1_L = self.main.gammas(t)[0]
+            gamma2_L = self.main.gammas(t)[1]
+            gamma3_L = self.main.gammas(t)[2]
+            gamma1_U = self.main.gammas(t)[3]
+            gamma2_U = self.main.gammas(t)[4]
+            gamma3_U = self.main.gammas(t)[5]
+
+            x_low = (lambda_low * gamma1_L + (1 - lambda_low) * gamma1_U)
+            y_low = (lambda_low * gamma2_L + (1 - lambda_low) * gamma2_U)
+            z_low = (lambda_low * gamma3_L + (1 - lambda_low) * gamma3_U)
+            constraint_low = z3.And(x_low<self.x2, x_low>self.x1, y_low<self.y2, y_low>self.y1, z_low<self.z2, z_low>self.z1)
+            all_constraints.append(constraint_low)
+
+            x_high = (lambda_high * gamma1_L + (1 - lambda_high) * gamma1_U)
+            y_high = (lambda_high * gamma2_L + (1 - lambda_high) * gamma2_U)
+            z_high = (lambda_high * gamma3_L + (1 - lambda_high) * gamma3_U)
+            constraint_high = z3.And(x_high<self.x2, x_high>self.x1, y_high<self.y2, y_high>self.y1, z_high<self.z2, z_high>self.z1)
+            all_constraints.append(constraint_high)
+
+        print("Added Stay Constraints: ", self.main.setpoints)
+        end = time.time()
+        self.main.displayTime(self.start, end)
+        return all_constraints
+    
+    def execute_stay_3D_depth_full(self):
+        self.main.setpoints.append([self.x1, self.x2, self.y1, self.y2, self.z1, self.z2, self.t1, self.t2])
+        all_constraints = []
+        t_values = np.arange(self.t1, self.t2, self.main._step)
+        for t in t_values:
+            for lambda_1 in self.main.lambda_values:
+                for lambda_2 in self.main.lambda_values:
+                    for lambda_3 in self.main.lambda_values:
+                        gamma1_L = self.main.gammas(t)[0]
+                        gamma2_L = self.main.gammas(t)[1]
+                        gamma3_L = self.main.gammas(t)[2]
+                        gamma1_U = self.main.gammas(t)[3]
+                        gamma2_U = self.main.gammas(t)[4]
+                        gamma3_U = self.main.gammas(t)[5]
+
+                        x = (lambda_1 * gamma1_L + (1 - lambda_1) * gamma1_U)
+                        y = (lambda_2 * gamma2_L + (1 - lambda_2) * gamma2_U)
+                        z = (lambda_3 * gamma3_L + (1 - lambda_3) * gamma3_U)
+                        constraint = z3.And(x<self.x2, x>self.x1, y<self.y2, y>self.y1, z<self.z2, z>self.z1)
+                        all_constraints.append(constraint)
+
         print("Added Stay Constraints: ", self.main.setpoints)
         end = time.time()
         self.main.displayTime(self.start, end)
